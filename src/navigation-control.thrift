2cf5d2fe5d4ce23a7923db3c0267368a901e8c3a
Andrea Serino <andrea.serino@iit.it>
2024-06-04 15:01:13 +0200

    conficts solved

--- src/navigation-control.thrift
+++ src/navigation-control.thrift
@@ -58,4 +58,19 @@ service navController_IDL
     * @return true if navigation is being currently performed.
     */
    bool is_navigating();
+   
+   /**
+    * Stop navigation.
+    * @return true/false on success/failure.
+    */
+   bool stop();
+
+   /**
+    * Reset odometry.
+    * @param x_0 can optionally become the current x-coordinate (meters).
+    * @param y_0 can optionally become the current y-coordinate (meters).
+    * @param theta_0 can optionally become the current theta-coordinate (degrees).
+    * @return true/false on success/failure.
+    */
+   bool reset_odometry(1:double x_0 = 0.0, 2:double y_0 = 0.0, 3:double theta_0 = 0.0);
 }
